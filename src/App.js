4405e53a5ce47e6b27d2f2f4f97a833fae71db8b
Raphael Isla <rpbisla0025@gmail.com>
2023-07-01 20:38:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btn:["AC", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="]
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App w-100 p-3">
+        <div className='calculator container-fluid border border-5 border-black p-0'>
+          <div className="row row-cols-4 w-100 m-0 border border-black">
+            <div className='col-12 '>sc1</div>
+            <div className='col-12'>sc</div>
+            
+              {
+                this.state.btn.map((value, indx) =>(
+                  <button key={indx} className={`col button btn btn-outline-secondary ${value === 'AC' ? 'col-9 btn-outline-danger' : value === '=' ? 'col-6 btn btn-outline-info' : ''}`}>{value}</button>
+                ))
+              }
+          </div>
+        </div>
+        <div className='h-25 w-100 mt-4' style={{backgroundColor: 'cyan'}}>
+          <p>Rp</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
